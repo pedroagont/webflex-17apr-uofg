207a2a749dad414496bdddaf7ee209fd25b134da
Pedro A. González <pgonzalez@post.com>
2023-09-02 15:24:08 -0500

    Add cypress example

---
+++ m08w20/react-albums/cypress/integration/03_text-input.spec.js
@@ -0,0 +1,19 @@
+describe('Text Input', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    // use `as` to alias vars and reference them with @varName
+    cy.get('.search__form').find('input').as('searchField');
+  });
+
+  it('accepts text input', () => {
+    cy.get('@searchField')
+      .type('Carrie Underwood', { delay: 100 })
+      .should('have.value', 'Carrie Underwood');
+  });
+
+  it('can handle backspace', () => {
+    cy.get('@searchField')
+      .type('Beee{backspace}ge{backspace}{backspace} Gees', { delay: 150 })
+      .should('have.value', 'Bee Gees');
+  });
+});
