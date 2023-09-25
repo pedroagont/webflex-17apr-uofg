29b386d3b841add9743282feb6464913c3c0ff1c
Pedro A. González <pgonzalez@post.com>
2023-09-25 14:23:34 -0500

    update notes and video link

---
+++ m09w23/README.md
@@ -0,0 +1,11 @@
+# M09W24 - Advanced lecture JWT Auth
+
+### To Do
+
+- [ ] What is JWT?
+- [ ] CRUD example app with simple auth
+- [ ] Implementing JWT
+- [ ] Discuss!
+
+### Video
+[https://vimeo.com/867536319/677464dfbf?share=copy](https://vimeo.com/867536319/677464dfbf?share=copy)
---
+++ m09w23/jwt-auth/app.js
@@ -0,0 +1,229 @@
+// ----------------------- REQUIREMENTS
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+// Database
+const fruitsDB = {
+  a1q: {
+    id: 'a1q',
+    name: 'mango',
+    color: 'yellow',
+    emoji: '🥭',
+  },
+  w4f: {
+    id: 'w4f',
+    name: 'grape',
+    color: 'purple',
+    emoji: '🍇',
+  },
+};
+
+const usersDB = [
+  {
+    id: 'ar5',
+    email: 'user@example.com',
+    password: 'abc123',
+  },
+];
+
+const verifyToken = (req, res, next) => {
+  // console.log(req.headers)
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(403).send({ message: 'Authorization header not valid' });
+  }
+
+  const token = authorization.split(' ')[1];
+  if (!token) {
+    return res.status(403).send({ message: 'Token does not exists' });
+  }
+
+  jwt.verify(token, SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: 'Token is not valid' });
+    }
+
+    next();
+  });
+};
+
+// ----------------------- SETUP AND MIDDLEWARES
+const app = express();
+const port = process.env.PORT || 3000;
+const SECRET = process.env.SECRET || 'MYSUPERSECRET123';
+
+app.use(helmet()); // includes security headers (owasp)
+app.use(morgan('dev')); // middleware that logs all the requests
+app.use(express.json()); // allow requests to include json body
+
+// ----------------------- ROUTES / ENDPOINTS
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World! 🐳</h1><p>CRUD /api/fruits</p>');
+});
+
+app.get('/home', (req, res) => {
+  res.status(200).send({ message: '🏡' });
+});
+
+// CRUD Operations
+// CREATE - POST
+app.post('/api/fruits', verifyToken, (req, res) => {
+  const { name, color, emoji } = req.body;
+  if (!name || !color || !emoji) {
+    return res
+      .status(400)
+      .send({ message: 'Provide name, color and emoji to create a fruit' });
+  }
+
+  let id = Math.random().toString(36).substr(2, 3);
+
+  fruitsDB[id] = {
+    id,
+    name,
+    color,
+    emoji,
+  };
+
+  res.status(201).send({ message: 'Created!', fruit: fruitsDB[id] });
+});
+
+// READ - GET
+// Read All
+app.get('/api/fruits', (req, res) => {
+  res.status(200).send({ message: 'List of all fruits!', fruits: fruitsDB });
+});
+
+// Read One
+app.get('/api/fruits/:id', (req, res) => {
+  const { id } = req.params;
+
+  const fruit = fruitsDB[id];
+  if (!fruit) {
+    return res.status(404).send({ message: 'Sorry, fruit not found' });
+  }
+
+  res.status(200).send({ message: 'Here is your fruit!', fruit });
+});
+
+// UPDATE - PUT
+app.put('/api/fruits/:id', verifyToken, (req, res) => {
+  const { name, color, emoji } = req.body;
+  if (!name || !color || !emoji) {
+    return res
+      .status(400)
+      .send({ message: 'Provide name, color and emoji to update a fruit' });
+  }
+
+  const { id } = req.params;
+
+  let fruit = fruitsDB[id];
+  if (!fruit) {
+    return res.status(404).send({ message: 'Sorry, fruit not found' });
+  }
+
+  fruitsDB[id] = {
+    id,
+    name,
+    color,
+    emoji,
+  };
+
+  res.status(201).send({ message: 'Updated!', fruit: fruitsDB[id] });
+});
+
+// DELETE - DELETE
+app.delete('/api/fruits/:id', verifyToken, (req, res) => {
+  const { id } = req.params;
+
+  let fruit = fruitsDB[id];
+  if (!fruit) {
+    return res.status(404).send({ message: 'Sorry, fruit not found' });
+  }
+
+  delete fruitsDB[id];
+  res.status(204).send();
+});
+
+// AUTHENTICATION ROUTES
+// Register
+app.post('/api/auth/register', (req, res) => {
+  // Body validation
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(400)
+      .send({ message: 'You need to provide email and password to register' });
+  }
+
+  // Email is not repeated validation
+  const emailExists = usersDB.find((u) => u.email === email);
+  if (emailExists) {
+    return res.status(400).send({ message: 'Email already exists' });
+  }
+
+  // Creating new user id
+  const id = Math.random().toString(36).substr(2, 3);
+
+  // Hashing password with bcrypt
+  const hashedPassword = bcrypt.hashSync(password, 8);
+
+  // Creating new user object
+  const newUser = {
+    id,
+    email,
+    password: hashedPassword,
+  };
+
+  // Adding the new user object into the DB
+  usersDB.push(newUser);
+  console.log(usersDB);
+
+  // Respond back to client
+  res.status(200).send({ message: 'User registered!', user: newUser });
+});
+
+// Login
+app.post('/api/auth/login', (req, res) => {
+  // Body validation
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(400)
+      .send({ message: 'You need to provide email and password to login' });
+  }
+
+  // Existing user validation
+  const user = usersDB.find((u) => u.email === email);
+  if (!user) {
+    return res.status(400).send({ message: 'Invalid credentials' });
+  }
+
+  // Passwords matching validation
+  const passwordsMatch = bcrypt.compareSync(password, user.password);
+  if (!passwordsMatch) {
+    return res.status(400).send({ message: 'Invalid credentials' });
+  }
+
+  // After all validations, we create the payload
+  const payload = {
+    id: user.id,
+    email: user.email,
+  };
+
+  // Generate token
+  const token = jwt.sign(payload, SECRET);
+
+  // Respond back to client
+  res.status(200).send({ message: 'Welcome!', token });
+});
+
+// Catch all route
+app.use((req, res) => {
+  res.status(404).send({ message: 'URL Not found' });
+});
+
+// ----------------------- LISTENER
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
